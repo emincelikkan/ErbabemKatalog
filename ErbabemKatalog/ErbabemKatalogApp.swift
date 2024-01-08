07794c0e002e73036b0911ab72f9808e9bbd71a2
emincelikkanmobven <emin.celikkan@mobven.com>
2024-01-08 19:30:05 +0300

    Initial Commit

---
+++ ErbabemKatalog/ErbabemKatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErbabemKatalogApp.swift
+//  ErbabemKatalog
+//
+//  Created by Emin Çelikkan on 8.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ErbabemKatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
